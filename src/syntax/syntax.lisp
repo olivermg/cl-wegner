92444aa9cd715768b897d98b4ee0e480109361e3
Oliver Wegner <void1976@gmail.com>
2013-10-08 20:05:33 +0200

    make defun/w work for required & rest arguments

--- src/syntax/syntax.lisp
+++ src/syntax/syntax.lisp
@@ -4,6 +4,10 @@
 
 (defvar *functions* (make-hash-table))
 
+(defstruct function/w
+  function
+  argcount)
+
 (defun get-required-params (all-args)
   (car (split-sequence:split-sequence
 	#\&
@@ -13,8 +17,37 @@
 			       (symbol-name arg))
 		     0)))))
 
+(defun filter-keywords-from-params (params)
+  (remove-if
+   (lambda (s)
+     (eql
+      (position #\&
+		(symbol-name s))
+      0))
+   params))
+
+(defun funcall-or-apply (arglist)
+  (if (some
+       (lambda (s)
+	 (eql
+	  (position #\&
+		    (symbol-name s))
+	  0))
+       arglist)
+      'apply
+      'funcall))
+
 (defmacro defun/w (name (&rest args) &body body)
   (setf (gethash name *functions*)
-	(length (get-required-params args)))
-  `(defun ,name (,@args)
-     ,@body))
+	(make-function/w
+	 :function (eval `(lambda (,@args)
+			    ,@body))
+	 :argcount (length (get-required-params args))))
+  (let ((args-without-keywords
+	 (filter-keywords-from-params args))
+	(call-fn
+	 (funcall-or-apply args)))
+    `(defun ,name (,@args)
+       (let* ((fn-s (gethash ',name *functions*))
+	      (fn (function/w-function fn-s)))
+	 (,call-fn fn ,@args-without-keywords)))))
